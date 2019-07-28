397a9e053a9d6aae1993a8cde4a989d643a22fb5
mithun <mithunj43@gmail.com>
2019-07-28 21:48:32 +0530

    test

--- src/Prac.java
+++ src/Prac.java
@@ -1,21 +1,36 @@
-class Singleton{
-    private static Singleton singleton;
-    private Singleton(){
-    }
-    public static Singleton getSingleton(){
-        if(singleton==null){
-            singleton=new Singleton();
-        }
-        return singleton;
-    }
-
-}
-
 public class Prac{
     public static void main(String[] args) {
-        Singleton singleton = Singleton.getSingleton();
-        System.out.println(singleton);
-        Singleton singleton1 = Singleton.getSingleton();
-        System.out.println(singleton1 );
+        System.out.println(eggDropping(2,10));
+    }
+
+
+    public static int eggDropping(int eggs,int floors){
+        int res=Integer.MAX_VALUE;
+        for(int i=1;i<=floors;i++){
+            int stepRes = _eggDropping(eggs,0,i,0,floors);
+            System.out.println(stepRes+" "+i);
+            if(stepRes<res){
+                res = stepRes;
+            }
+        }
+        return res;
+    }
+
+    private static int _eggDropping(int eggs,int prevFloor,int presFloor,int tries,int floors){
+        if(eggs == 1){
+            return presFloor-prevFloor-1+tries;
+        }else if(presFloor==floors){
+            return tries;
+        }else {
+            int res = Integer.MIN_VALUE;
+            int i=1;
+            while (i+presFloor<=floors){
+                res = Math.max(_eggDropping(eggs-1,presFloor,presFloor+i,tries+1,floors),_eggDropping(eggs,presFloor
+                        ,presFloor+i,tries+1,floors));
+                i++;
+            }
+            return res;
+
+        }
     }
 }
