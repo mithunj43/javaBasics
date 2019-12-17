d7c347877aebea93fa52bbf4a266cb56c7fc1552
mithun <mithunj43@gmail.com>
2019-12-17 13:43:33 +0530

    ok cool

--- src/Prac.java
+++ src/Prac.java
@@ -1,36 +1,30 @@
+import java.util.Arrays;
+
 public class Prac{
     public static void main(String[] args) {
-        System.out.println(eggDropping(2,10));
+        int arr [] = {45, 12, 33, 445, 867, 999, 23, 25};
+        System.out.println(Arrays.toString(arr));
+        selectionSort(arr,0);
+        System.out.println(Arrays.toString(arr));
     }
 
-
-    public static int eggDropping(int eggs,int floors){
-        int res=Integer.MAX_VALUE;
-        for(int i=1;i<=floors;i++){
-            int stepRes = _eggDropping(eggs,0,i,0,floors);
-            System.out.println(stepRes+" "+i);
-            if(stepRes<res){
-                res = stepRes;
+    public static void selectionSort(int arr[] , int index){
+        if(index==arr.length)return;
+        else {
+            int ele = arr[index];
+            int swapIndex = -1;
+            for (int i = index+1; i < arr.length ; i++) {
+                if(arr[i]<ele){
+                    swapIndex=i;
+                    ele=arr[i];
+                }
             }
-        }
-        return res;
-    }
-
-    private static int _eggDropping(int eggs,int prevFloor,int presFloor,int tries,int floors){
-        if(eggs == 1){
-            return presFloor-prevFloor-1+tries;
-        }else if(presFloor==floors){
-            return tries;
-        }else {
-            int res = Integer.MIN_VALUE;
-            int i=1;
-            while (i+presFloor<=floors){
-                res = Math.max(_eggDropping(eggs-1,presFloor,presFloor+i,tries+1,floors),_eggDropping(eggs,presFloor
-                        ,presFloor+i,tries+1,floors));
-                i++;
+            if(swapIndex!=-1){
+                int swapHelper = arr[index];
+                arr[index] = arr[swapIndex];
+                arr[swapIndex] = swapHelper;
             }
-            return res;
-
+            selectionSort(arr,index+1);
         }
     }
 }
