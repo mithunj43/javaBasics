666fba866da980ca31dd3d76d5501902c0190bc2
mithun <mithunj43@gmail.com>
2019-12-24 13:24:37 +0530

    binary tree

--- src/Prac.java
+++ src/Prac.java
@@ -50,6 +50,45 @@ class BinaryTree{
             }
         }
     }
+    public void inOrderTraversal(){
+        _inOrderTraversal(root);
+    }
+
+    private void _inOrderTraversal(Node node){
+        if(node == null)return;
+        else {
+            _inOrderTraversal(node.left);
+            System.out.print(node.data+" ");
+            _inOrderTraversal(node.right);
+        }
+    }
+
+    public void preOrderTraversal(){
+        _preOrderTraversal(root);
+    }
+
+    private void _preOrderTraversal(Node node){
+        if(node == null)return;
+        else {
+            System.out.print(node.data+" ");
+            _preOrderTraversal(node.left);
+            _preOrderTraversal(node.right);
+        }
+    }
+    public void postOrderTraversal(){
+        _postOrderTraversal(root);
+    }
+    private void _postOrderTraversal(Node node){
+        if(node == null)return;
+        else {
+            _postOrderTraversal(node.left);
+            _postOrderTraversal(node.right);
+            System.out.print(node.data+" ");
+
+        }
+    }
+
+
 }
 
 public class Prac{
@@ -61,5 +100,12 @@ public class Prac{
         binaryTree.insertDataPls(4);
         binaryTree.insertDataPls(5);
         binaryTree.levelOrderTraversal();
+        System.out.println();
+        binaryTree.inOrderTraversal();
+        System.out.println();
+        binaryTree.preOrderTraversal();
+        System.out.println();
+        binaryTree.postOrderTraversal();
+        System.out.println();
     }
 }
