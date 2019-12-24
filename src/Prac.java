99f0788c89d336a79251541bbfdc3be1a8124768
mithun <mithunj43@gmail.com>
2019-12-24 13:11:28 +0530

    binary tree

--- src/Prac.java
+++ src/Prac.java
@@ -1,30 +1,29 @@
-import java.util.Arrays;
+class Node{
+    int data;
+    Node left,right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree{
+    Node root;
+    BinaryTree(int data){
+        root = new Node(data);
+    }
+    BinaryTree(){
+
+    }
+}
 
 public class Prac{
     public static void main(String[] args) {
-        int arr [] = {45, 12, 33, 445, 867, 999, 23, 25};
-        System.out.println(Arrays.toString(arr));
-        selectionSort(arr,0);
-        System.out.println(Arrays.toString(arr));
-    }
-
-    public static void selectionSort(int arr[] , int index){
-        if(index==arr.length)return;
-        else {
-            int ele = arr[index];
-            int swapIndex = -1;
-            for (int i = index+1; i < arr.length ; i++) {
-                if(arr[i]<ele){
-                    swapIndex=i;
-                    ele=arr[i];
-                }
-            }
-            if(swapIndex!=-1){
-                int swapHelper = arr[index];
-                arr[index] = arr[swapIndex];
-                arr[swapIndex] = swapHelper;
-            }
-            selectionSort(arr,index+1);
-        }
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.root = new Node(1);
+        binaryTree.root.left = new Node(2);
+        binaryTree.root.right = new Node(3);
     }
 }
