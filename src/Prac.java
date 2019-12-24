8ebe848f9b75d484f013a58200a4d1caee28d25d
mithun <mithunj43@gmail.com>
2019-12-24 13:19:30 +0530

    binary tree

--- src/Prac.java
+++ src/Prac.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 class Node{
     int data;
     Node left,right;
@@ -17,13 +19,47 @@ class BinaryTree{
     BinaryTree(){
 
     }
+    public void levelOrderTraversal(){
+        if(root!=null){
+            LinkedList<Node> nodes = new LinkedList<>();
+            nodes.add(root);
+            while (!nodes.isEmpty()){
+                Node tmpNode = nodes.poll();
+                System.out.print(tmpNode.data+" ");
+                if(tmpNode.left != null) nodes.add(tmpNode.left);
+                if(tmpNode.right != null) nodes.add(tmpNode.right);
+            }
+        }
+    }
+    public void insertDataPls(int data){
+        if(root == null){
+            root = new Node(data);
+        }else {
+            LinkedList<Node> nodes = new LinkedList<>();
+            nodes.add(root);
+            while (!nodes.isEmpty()){
+                Node tmpNode = nodes.poll();
+                if(tmpNode.left == null){
+                    tmpNode.left = new Node(data);
+                    return;
+                }else nodes.add(tmpNode.left);
+                if(tmpNode.right == null){
+                    tmpNode.right = new Node(data);
+                    return;
+                }else nodes.add(tmpNode.right);
+            }
+        }
+    }
 }
 
 public class Prac{
     public static void main(String[] args) {
         BinaryTree binaryTree = new BinaryTree();
-        binaryTree.root = new Node(1);
-        binaryTree.root.left = new Node(2);
-        binaryTree.root.right = new Node(3);
+        binaryTree.insertDataPls(1);
+        binaryTree.insertDataPls(2);
+        binaryTree.insertDataPls(3);
+        binaryTree.insertDataPls(4);
+        binaryTree.insertDataPls(5);
+        binaryTree.levelOrderTraversal();
     }
 }
